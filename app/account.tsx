e86df56ecf7ef719ce94f473d632eab1a74b3e81
helloworld131 <t7401113@gmail.com>
2025-08-30 20:36:00 +0200

    v1.1.8

--- app/account.tsx
+++ app/account.tsx
@@ -1,23 +1,13 @@
-import { useRouter } from "expo-router";
 import { t } from "i18next";
-import {
-  ChevronLeft
-} from "lucide-react-native";
 import * as React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from "react-native-safe-area-context";
 import "./i18n.js";
 
-
-
 const account = () => {
-  const router = useRouter();
 
   return (
     <SafeAreaView>
-    <TouchableOpacity style={styles.placeholder} onPress={() => router.navigate("/profilescreen")}>
-      <ChevronLeft />
-    </TouchableOpacity>
     <View style={styles.container}>
       <Text style={styles.text}>{t('not_signed_in')}</Text>
       <Text>{t('signing_in_not_ready')}.</Text>
@@ -33,14 +23,10 @@ export default account ;
 const styles = StyleSheet.create({
   container: {
     justifyContent: 'center',
-    flex: 1,
     alignItems: 'center',
+    flexBasis: 'auto',
   },
   text: {
     fontSize: 23
-  },
-  placeholder: {
-    marginTop: 20,
-    marginLeft: 23,
   },
 });
--- app/profilescreen.tsx
+++ app/profilescreen.tsx
@@ -1,22 +1,22 @@
 import { useRouter } from "expo-router";
 import {
-    Bolt,
-    Bug,
-    ChevronRight,
-    Info,
-    List,
-    TestTube,
-    User,
-    Users,
+  Bolt,
+  Bug,
+  ChevronRight,
+  Info,
+  List,
+  TestTube,
+  User,
+  Users,
 } from "lucide-react-native";
 import React from "react";
 import { useTranslation } from "react-i18next";
 import {
-    SafeAreaView,
-    StyleSheet,
-    Text,
-    TouchableOpacity,
-    View,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
 } from "react-native";
 import "./i18n.js";
 
@@ -27,126 +27,120 @@ function ProfileScreen() {
 
   return (
     <SafeAreaView style={styles.all}>
-      <View style={styles.placeholder} />
-      <TouchableOpacity
-        style={styles.page}
-        onPress={() => router.navigate("/account")}
-      >
-        <User style={styles.icon} />
-        <Text style={styles.link}>{t("profile")}</Text>
-        <ChevronRight style={styles.arrow1} />
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.page}
-        onPress={() => router.navigate("/settings")}
-      >
-        <Bolt style={styles.icon} />
-        <Text style={styles.link}>{t("settings")}</Text>
-        <ChevronRight style={styles.arrow2} />
-      </TouchableOpacity>
-      <View style={styles.placeholderk} />
-      <View style={styles.line} />
-      <TouchableOpacity
-        style={styles.page}
-        onPress={() => router.navigate("/updatelog")}
-      >
-        <List style={styles.icon} />
-        <Text style={styles.link}>{t("update_log")}</Text>
-        <ChevronRight style={styles.arrow3} />
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.page}
-        onPress={() => router.navigate("/about")}
-      >
-        <Info style={styles.icon} />
-        <Text style={styles.link}>{t("about_gps")}</Text>
-        <ChevronRight style={styles.arrow4} />
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.page}
-        onPress={() => router.navigate("/bugreport")}
-      >
-        <Bug style={styles.icon} />
-        <Text style={styles.link}>{t("bug_report")}</Text>
-        <ChevronRight style={styles.arrow5} />
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.page}
-        onPress={() => router.navigate("/invite")}
-      >
-        <Users style={styles.icon} />
-        <Text style={styles.link}>{t("invite")}</Text>
-        <ChevronRight style={styles.arrow7} />
-      </TouchableOpacity>
-      <View style={styles.placeholderk} />
-      <View style={styles.line} />
-      <TouchableOpacity
-        style={styles.page}
-        onPress={() => router.navigate("/test")}
-      >
-        <TestTube style={styles.icon} />
-        <Text style={styles.link}>{t("test")}</Text>
-        <ChevronRight style={styles.arrow6} />
-      </TouchableOpacity>
+      <View>
+        <View style={styles.placeholder} />
+        <TouchableOpacity
+          style={styles.page}
+          onPress={() => router.navigate("/account")}
+        >
+          <User style={styles.icon} />
+          <Text style={styles.link}>{t("profile")}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.page}
+          onPress={() => router.navigate("/settings")}
+        >
+          <Bolt style={styles.icon} />
+          <Text style={styles.link}>{t("settings")}</Text>
+        </TouchableOpacity>
+        <View style={styles.line} />
+        <TouchableOpacity
+          style={styles.page}
+          onPress={() => router.navigate("/updatelog")}
+        >
+          <List style={styles.icon} />
+          <Text style={styles.link}>{t("update_log")}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.page}
+          onPress={() => router.navigate("/about")}
+        >
+          <Info style={styles.icon} />
+          <Text style={styles.link}>{t("about_gps")}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.page}
+          onPress={() => router.navigate("/bugreport")}
+        >
+          <Bug style={styles.icon} />
+          <Text style={styles.link}>{t("bug_report")}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.page}
+          onPress={() => router.navigate("/invite")}
+        >
+          <Users style={styles.icon} />
+          <Text style={styles.link}>{t("invite")}</Text>
+        </TouchableOpacity>
+        <View style={styles.line} />
+        <TouchableOpacity
+          style={styles.page}
+          onPress={() => router.navigate("/test")}
+        >
+          <TestTube style={styles.icon} />
+          <Text style={styles.link}>{t("test")}</Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <View style={styles.placeholder} />
+        <TouchableOpacity onPress={() => router.navigate("/account")}>
+        <ChevronRight style={styles.arrow} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => router.navigate("/settings")}>
+        <ChevronRight style={styles.arrow} />
+        </TouchableOpacity>
+        <View style={styles.line} />
+        <TouchableOpacity onPress={() => router.navigate("/updatelog")}>
+        <ChevronRight style={styles.arrow} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => router.navigate("/about")}>
+        <ChevronRight style={styles.arrow} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => router.navigate("/bugreport")}>
+        <ChevronRight style={styles.arrow} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => router.navigate("/invite")}>
+        <ChevronRight style={styles.arrow} />
+        </TouchableOpacity>
+        <View style={styles.line} />
+        <TouchableOpacity onPress={() => router.navigate("/test")}>
+        <ChevronRight style={styles.arrow} />
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  all: {
+    flexDirection: "row",
+  },
+  placeholder: {
+    marginTop: 34,
+  },
   line: {
     height: 1,
     backgroundColor: "#ccc",
     alignSelf: "stretch",
     marginVertical: 12,
-    marginHorizontal: 17,
+    marginHorizontal: 0,
   },
-  page: {
-    flexDirection: "row",
-    marginLeft: 35,
-    marginTop: 17,
-    alignItems: "center",
-  },
+
   link: {
     fontSize: 21,
     fontWeight: "600",
   },
-  icon: {
-    marginRight: 28,
-    marginVertical: 8,
-    flexDirection: "column",
+  page: {
+    flexDirection: "row",
+    alignItems: "center",
   },
-  placeholder: {
-    marginTop: 23,
+  icon: {
+    marginHorizontal: 23,
+    marginVertical: 23,
   },
   arrow: {
-    marginHorizontal: 70,
-  },
-  all: {
-    flexDirection: "column",
-  },
-  arrow1: {
-    marginLeft: 168,
-  },
-  arrow2: {
-    marginLeft: 154,
-  },
-  arrow3: {
-    marginLeft: 154,
-  },
-  arrow4: {
-    marginLeft: 129,
-  },
-  arrow5: {
-    marginLeft: 126,
-  },
-  arrow6: {
-    marginLeft: 196,
-  },
-  arrow7: {
-    marginLeft: 103,
-  },
-  placeholderk: {
-    marginVertical: 10,
+    paddingHorizontal: 130,
+    marginVertical: 23,
   },
 });
 
